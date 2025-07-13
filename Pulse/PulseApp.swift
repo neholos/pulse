89ea2f7d7e702f97d341b0933c223bfc66dcb3bf
Sasha Jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2025-07-13 14:33:55 +0300

    Initial Commit

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Sasha Jaroshevskii on 13.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
